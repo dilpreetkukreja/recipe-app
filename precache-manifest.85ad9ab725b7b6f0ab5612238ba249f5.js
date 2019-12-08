ee32c746b8fbb7bde989533265a4987e854801df
dilpreet kaur <dilpreet.kukreja@gmail.com>
2019-12-08 02:59:47 -0800

    Updates

---
+++ precache-manifest.85ad9ab725b7b6f0ab5612238ba249f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a80bfc54dc4604bb08b39ecf1ecc13f",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "456d460d23f17171ff49",
+    "url": "/recipe-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "261df75022378385794a",
+    "url": "/recipe-app/static/css/main.b10db12e.chunk.css"
+  },
+  {
+    "revision": "456d460d23f17171ff49",
+    "url": "/recipe-app/static/js/2.db5be3e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe-app/static/js/2.db5be3e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "261df75022378385794a",
+    "url": "/recipe-app/static/js/main.d9f021c4.chunk.js"
+  },
+  {
+    "revision": "c9e78abdeae374382543",
+    "url": "/recipe-app/static/js/runtime-main.c7667b32.js"
+  }
+]);
